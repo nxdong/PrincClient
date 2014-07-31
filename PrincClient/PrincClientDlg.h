403ed832adb491543c84eb3e60c1c716b56415df
Principle <qq357478904@gmail.com>
2014-07-31 21:52:11 +0800

    just a start

---
+++ PrincClient/PrincClientDlg.h
@@ -0,0 +1,34 @@
+
+// PrincClientDlg.h : 头文件
+//
+
+#pragma once
+#include "ClientSocket.h"
+
+// CPrincClientDlg 对话框
+class CPrincClientDlg : public CDialogEx
+{
+// 构造
+public:
+	CPrincClientDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+	enum { IDD = IDD_PRINCCLIENT_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+	CClientSocket m_clientSocket;
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton1();
+};
